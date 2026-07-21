d5dc3ad2917528785a4afe99bd3f1197de9fa5eb
shivaditya232 <shivaditya2503@gmail.com>
2026-07-21 18:24:37 +0530

    created user schema

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose=require('mongoose');
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['admin','team'],
+        required:true
+    },
+    teamName:{
+        type:String,
+    },
+    purse:{
+        type:Number,
+        default:100
+    },
+    purseRemaining:{
+        type:Number,
+        default:100
+    },
+
+},{
+    timestamps:true
+});
+
+module.exports=mongoose.model('User',UserSchema);
